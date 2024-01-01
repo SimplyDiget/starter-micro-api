6df540d32cc3dd3c4eb2280ede182ff0b5c07124
SimplyDiget <154851769+SimplyDiget@users.noreply.github.com>
2023-12-31 21:57:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/proxy', async (req, res) => {
+  const targetUrl = req.query.url;
+
+  try {
+    const proxyResponse = await fetch(targetUrl);
+    const text = await proxyResponse.text();
+    res.send(text);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Error occurred while making the request.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
